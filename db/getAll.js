fc4da99634e29ae01d7dd7f6bc403716a60ac75e
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-12-13 02:03:01 +0100

    Update getAll.js

--- db/getAll.js
+++ db/getAll.js
@@ -1,7 +1,12 @@
-const fs = require("fs/promises");
+const fs = require('fs/promises');
+const path = require('path');
+
+const contactsPath = require('./contactsPath');
 
 const getAll = async () => {
-  const contacts = await fs.readFile("contacts.json");
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+
   return contacts;
 };
 
